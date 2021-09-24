73d43e46988f6240cdacd8ec0197a30df39ec69e
Fajar Anggara <fanggara@tibco.com>
2021-09-24 19:53:55 +0700

    Rename Activity Struct

--- activity/ope/getoffers/activity.go
+++ activity/ope/getoffers/activity.go
@@ -2,16 +2,17 @@ package getoffers
 
 import (
 	"github.com/project-flogo/core/activity"
+	"github.com/project-flogo/core/data/metadata"
 )
 
-/*func init() {
-	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
-}*/
+func init() {
+	_ = activity.Register(&MyActivity{},New) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
+}
 
-//var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
+var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
 
 //New optional factory method, should be used if one activity instance per configuration is desired
-/*func New(ctx activity.InitContext, settings ...interface{}) (activity.Activity, error) {
+func New(ctx activity.InitContext) (activity.Activity, error) {
 
 	s := &Settings{}
 	err := metadata.MapToStruct(ctx.Settings(), s, true)
@@ -21,27 +22,26 @@ import (
 
 	ctx.Logger().Debugf("Setting: %s", s.ASetting)
 
-	act := &Activity{} //add aSetting to instance
+	act := &MyActivity{} //add aSetting to instance
 
 	return act, nil
-}*/
-
-func NewActivity(metadata *activity.Metadata) activity.Activity {
-	return &Activity{metadata: metadata}
 }
 
-// Activity is an sample Activity that can be used as a base to create a custom activity
-type Activity struct {
-	metadata *activity.Metadata
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{}
+}
+
+// MyActivity is an sample MyActivity that can be used as a base to create a custom activity
+type MyActivity struct {
 }
 
 // Metadata returns the activity's metadata
-func (a *Activity) Metadata() *activity.Metadata {
-	return a.metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return activityMd
 }
 
 // Eval implements api.Activity.Eval - Logs the Message
-func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+func (a *MyActivity) Eval(ctx activity.Context) (done bool, err error) {
 
 	input := &Input{}
 	err = ctx.GetInputObject(input)
